fa3522f061101c272271b55b4b620a784bcd8a71
Steve Ruben <SteveRuben@Gmail.com>
2012-10-11 10:56:54 -0500

    Interfaces.

---
+++ SquirrelDb.Interfaces/IIndexer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SquirrelDb.Interfaces
+{
+    public interface IIndexer
+    {
+        string Name { get; }
+
+        Func<string, bool> IndexDocument { get; }
+
+        List<string> PostProcessing(List<string> documents, Dictionary<string,string> arguments);
+
+    }
+}
